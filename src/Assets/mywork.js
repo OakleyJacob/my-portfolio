66052b8f1e917e2207e25eca902e42f2afb65ddc
LEARN Academy <learnacademy@MacBook-Air-2.local>
2023-09-07 21:43:41 -0500

    added projects

--- src/Assets/mywork.js
+++ src/Assets/mywork.js
@@ -1,6 +1,7 @@
-import tictactoe from '../Assets/TicTacToe.png'
-import treasureHunt from '../Assets/treasure-hunt.png'
-
+import tictactoe from './TicTacToe.png'
+import treasureHunt from './treasure-hunt.png'
+import ers from './ers.png'
+import kw from './keepwagging.png'
 const myWork = [
     {name: 'Tic-Tac-Toe', 
     description: 'This app allows users to play tic-tac-toe alone or with a partner. It also allows users to select from four difficulty levels. It was built entirely in React with Html, CSS, and JS.',
@@ -12,15 +13,20 @@ const myWork = [
     link: 'https://my-treasure-hunt.vercel.app/',
     skills:  ['React', 'HTML', 'CSS', 'Javascript'],
     pic: treasureHunt},
-    {name: 'Lorem ipsum', 
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu justo ac lacus egestas auctor. ',
-    link: 'Lorem Link',
-    skills: ['Lorem', 'ipsum']},
-    {name: 'Lorem ipsum', 
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eu justo ac lacus egestas auctor. ',
-    link: 'Lorem Link',
-    skills: ['Lorem', 'ipsum']}
+    {name: 'Keep Wagging', 
+    description: 'A full stack application that allows a user to create an account, log in, and utilize full CRUD functionality with persistent data. ',
+    link: 'https://www.youtube.com/watch?v=uLhmRD17Kbo',
+    skills: ['Ruby on Rails', 'React', 'PostgreSQL'],
+    pic: kw
+},
 
+    {name: 'Ticket Reimbursement system', 
+    description: 'A backend, or API, that features multiple user roles, advanced filtering and searching, and full CRUD.',
+    link: 'https://github.com/OakleyJacob/java-api',
+    skills: ['Java', 'PostgreSQL'],
+    pic: ers
+}
+    
 ]
 
 export default myWork
